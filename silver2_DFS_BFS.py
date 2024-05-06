6e96a1648190c1ed7dedfeda8722228f3ce570a1
hwlee9905 <9905hyun@naver.com>
2024-05-07 01:19:18 +0900

    실버2, DFS와 BFS, solved

--- silver2_DFS_BFS.py
+++ silver2_DFS_BFS.py
@@ -35,5 +35,58 @@
 # 예제 출력 3
 # 1000 999
 # 1000 999
-import sys
-input = sys.stdin.readline()
+import sys, collections
+deque = collections.deque
+input = sys.stdin.readline
+N, M, V = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+
+
+
+def bfs(graph, start):
+    visitSequence = []
+    queue = deque([start])
+    visited = [False for _ in range(N+1)]
+    visited[start] = True
+
+    while queue:
+        node = queue.popleft()
+        visitSequence.append(node)
+        for i in sorted(graph[node]):
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+
+    return visitSequence
+
+def dfs(graph, start):
+    visitSequence = []
+    stack = [start]
+    visited = [False for _ in range(N+1)]
+    visited[start] = True
+
+    while stack:
+        node = stack.pop()
+        visitSequence.append(node)
+
+        for i in reversed(sorted(graph[node])):
+            if not visited[i]:
+                visited[i] = True
+                stack.append(i)
+
+    return visitSequence
+
+
+
+for n in dfs(graph, V):
+    print(n, end=" ")
+
+print()
+
+for n in bfs(graph, V):
+    print(n, end=" ")
