2e14e4f42444d159a639f1b7ac980c1b560fda88
hwlee9905 <9905hyun@naver.com>
2024-11-02 23:37:54 +0900

    silver3_virush input init

--- silver3_virus.py
+++ silver3_virus.py
@@ -1,9 +1,3 @@
-import sys, collections
-deque = collections.deque
-input = sys.stdin.readline
-
-
-
 # 문제
 # https://www.acmicpc.net/problem/2606
 
@@ -19,3 +13,19 @@ input = sys.stdin.readline
 
 # 예제 출력 1
 # 4
+
+import sys, collections
+deque = collections.deque
+input = sys.stdin.readline
+
+# N: 컴퓨터의 수, M: 연결되어 있는 컴퓨터 쌍의 수
+N, M = map(int, input().split())
+
+# 그래프를 인접 리스트로 표현
+graph = [[] for _ in range(N+1)]
+
+# 간선 정보를 입력받아 그래프에 추가
+for _ in range(M):
+    x, y = map(int, input().split())
+    graph[x].append(y)
+    graph[y].append(x)
