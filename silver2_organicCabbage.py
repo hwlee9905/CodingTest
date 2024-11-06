3a71a9c6bb49ddccf5c5b91c82799bb9d58f2b2f
edwin <edwin@furence.com>
2024-11-06 18:31:32 +0900

    ing: 1012번 진행상황 save

--- silver2_organicCabbage.py
+++ silver2_organicCabbage.py
@@ -6,31 +6,42 @@ input = sys.stdin.readline
 deque = collections.deque
 
 T = int(input())
+results = []
+
 for _ in range(T):
+    count = 0
     M, N, K = map(int, input().split())
-    List = [[] for _ in range(K)]
-    array = [[0] * M for _ in range(N)]
-    visited = [[False] * M for _ in range(N)]
+    array = [[0] * (M + 2) for _ in range(N + 2)]
+    visited = [[False] * (M + 2) for _ in range(N + 2)]
 
     for i in range(K):
         m, n = map(int, input().split())
         array[n][m] = 1
 
-    queue = deque([])
+    for i in range(1, N + 1):
+        for j in range(1, M + 1):
+            if array[i][j] and not visited[i][j]:
+                queue = deque([(i, j)])
+                visited[i][j] = True
+                while queue:
+                    x, y = queue.popleft()
+                    if array[x][y + 1] and not visited[x][y + 1]:
+                        queue.append((x, y + 1))
+                        visited[x][y + 1] = True
+                    if array[x][y - 1] and not visited[x][y - 1]:
+                        queue.append((x, y - 1))
+                        visited[x][y - 1] = True
+                    if array[x + 1][y] and not visited[x + 1][y]:
+                        queue.append((x + 1, y))
+                        visited[x + 1][y] = True
+                    if array[x - 1][y] and not visited[x - 1][y]:
+                        queue.append((x - 1, y))
+                        visited[x - 1][y] = True
+                count += 1
+    results.append(count)
 
-    for i, row in enumerate(visited):
-        for j, column in enumerate(row):
-            visited[i][j] = True
-            if column:
-                queue. test
-
-
-
-    print()
-    for row in array:
-        print(row)
-
-
+for result in results:
+    print(result)
 
 
 
