abd893a4ddd75133e3548fa50c080eb84c05a70e
hwlee <9905hyun@naver.com>
2024-11-10 01:27:13 +0900

    solved: 1012번

--- silver2_organicCabbage.py
+++ silver2_organicCabbage.py
@@ -11,37 +11,80 @@ results = []
 for _ in range(T):
     count = 0
     M, N, K = map(int, input().split())
-    array = [[0] * (M + 2) for _ in range(N + 2)]
-    visited = [[False] * (M + 2) for _ in range(N + 2)]
+    array = [[0] * (M) for _ in range(N)]
+    visited = [[False] * (M) for _ in range(N)]
 
     for i in range(K):
         m, n = map(int, input().split())
         array[n][m] = 1
 
-    for i in range(1, N + 1):
-        for j in range(1, M + 1):
+    for i in range(N):
+        for j in range(M):
             if array[i][j] and not visited[i][j]:
                 queue = deque([(i, j)])
                 visited[i][j] = True
                 while queue:
                     x, y = queue.popleft()
-                    if array[x][y + 1] and not visited[x][y + 1]:
-                        queue.append((x, y + 1))
-                        visited[x][y + 1] = True
-                    if array[x][y - 1] and not visited[x][y - 1]:
-                        queue.append((x, y - 1))
-                        visited[x][y - 1] = True
-                    if array[x + 1][y] and not visited[x + 1][y]:
-                        queue.append((x + 1, y))
-                        visited[x + 1][y] = True
-                    if array[x - 1][y] and not visited[x - 1][y]:
-                        queue.append((x - 1, y))
-                        visited[x - 1][y] = True
+                    dx = x - 1
+                    if 0 <= dx < N and not visited[dx][y] and array[dx][y]:
+                        queue.append((dx, y))
+                        visited[dx][y] = True
+                    dy = y - 1
+                    if 0 <= dy < M and not visited[x][dy] and array[x][dy]:
+                        queue.append((x, dy))
+                        visited[x][dy] = True
+                    dx = x + 1
+                    if 0 <= dx < N and not visited[dx][y] and array[dx][y]:
+                        queue.append((dx, y))
+                        visited[dx][y] = True
+                    dy = y + 1
+                    if 0 <= dy < M and not visited[x][dy] and array[x][dy]:
+                        queue.append((x, dy))
+                        visited[x][dy] = True
+
                 count += 1
     results.append(count)
 
 for result in results:
     print(result)
 
-
+# import collections
+# import sys
+# input = sys.stdin.readline
+# deque = collections.deque
+#
+# T = int(input())
+# results = []
+#
+# # 방향 벡터 (상, 하, 좌, 우)
+# dx = [-1, 1, 0, 0]
+# dy = [0, 0, -1, 1]
+#
+# for _ in range(T):
+#     count = 0
+#     M, N, K = map(int, input().split())
+#     array = [[0] * (M) for _ in range(N)]
+#     visited = [[False] * (M) for _ in range(N)]
+#
+#     for i in range(K):
+#         m, n = map(int, input().split())
+#         array[n][m] = 1
+#
+#     for i in range(N):
+#         for j in range(M):
+#             if array[i][j] and not visited[i][j]:
+#                 queue = deque([(i, j)])
+#                 visited[i][j] = True
+#                 while queue:
+#                     x, y = queue.popleft()
+#                     for d in range(4):
+#                         nx, ny = x + dx[d], y + dy[d]
+#                         if 0 <= nx < N and 0 <= ny < M and array[nx][ny] and not visited[nx][ny]:
+#                             queue.append((nx, ny))
+#                             visited[nx][ny] = True
+#                 count += 1
+#     results.append(count)
+#
+# for result in results:
+#     print(result)
 
