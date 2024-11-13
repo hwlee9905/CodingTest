a813df3e6522f37b8b6d867fe9b1afaac3e69dd1
hwlee <9905hyun@naver.com>
2024-11-13 22:46:57 +0900

    ing: 2583번 input

--- silver1_areaCalc.py
+++ silver1_areaCalc.py
@@ -3,3 +3,12 @@
 import sys
 
 input = sys.stdin.readline
+M, N, K = map(int, input().split())
+array = [[False] * (N+1) for _ in range(M+1)]
+
+for _ in range(K):
+    x1, y1, x2, y2 = map(int, input().split())
+    for x in range(x1, x2):
+        for y in range(y1, y2):
+            array[x][y] = True
+    
