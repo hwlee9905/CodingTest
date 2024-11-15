7e1f44981a4320568b24ee1a25678f3a8a96a99e
hwlee <9905hyun@naver.com>
2024-11-15 23:44:54 +0900

    ing: 2583번 진행상황 저장

--- silver1_areaCalc.py
+++ silver1_areaCalc.py
@@ -1,14 +1,44 @@
 # 문제
 # https://www.acmicpc.net/problem/2583
-import sys
-
+import sys, collections
+deque = collections.deque
 input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, 1, -1]
+count = 0
 M, N, K = map(int, input().split())
-array = [[False] * (N+1) for _ in range(M+1)]
+array = [[True] * (N+1) for _ in range(M+1)] # N = 가로길이 M= 세로길이
+visited = [[False] * (N+1) for _ in range(M+1)]
+areaList = []
 
 for _ in range(K):
     x1, y1, x2, y2 = map(int, input().split())
     for x in range(x1, x2):
         for y in range(y1, y2):
-            array[x][y] = True
-    
+            array[y][x] = False
+
+
+for y in range(M):
+    for x in range(N):
+        if not visited[y][x]:
+            count = 0
+            stack = deque([(x, y)])
+            while stack:
+                xStack, yStack = stack.pop()
+                count += 1
+                for vector in range(4):
+                    xVector = xStack + dx[vector]
+                    yVector = yStack + dy[vector]
+                    if (N >= xVector > 0) and (M >= yVector > 0) and not visited[yVector][xVector]:
+                        visited[yVector][xVector] = True
+                        stack.append((xVector, yVector))
+            areaList.append(count)
+
+for y in range(M):
+    for x in range(N):
+        print(array[y][x], end=" ")
+    print()
+
+for area in areaList:
+    print(area, end=" ")
