a17cb06c11218f16011f7ab8e35ef407b8706988
hwlee <9905hyun@naver.com>
2024-11-17 19:17:32 +0900

    solved: 1010번

--- silver5_place_the_bridge.py
+++ silver5_place_the_bridge.py
@@ -3,7 +3,29 @@
 import sys
 input = sys.stdin.readline
 
+def combination(n, r):
+    return int(factorial(n) / (factorial(r) * factorial(n-r)))
+
+def factorial(n):
+    if n == 0:
+        return 1
+
+    return n * factorial(n - 1)
+
 T = int(input()) # Test CASE N
+cases = []
+case = 0
 
 for _ in range(T):
     N, M = map(int, input().split())
+    cases.append(combination(M, N))
+
+for i in cases:
+    print(i, end=" ")
+
+
+
+
+
+
+
