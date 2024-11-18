bd2f833bc541a4cd5aab9e68857616b6c54f37c1
edwin <edwin@furence.com>
2024-11-18 16:59:10 +0900

    solved: 10810번

--- bronze3_throw_ball.py
+++ bronze3_throw_ball.py
@@ -1,7 +1,14 @@
+# 문제
+# https://www.acmicpc.net/problem/10810
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-
+list = [0 for _ in range(N)]
 for _ in range(M):
     i, j, k = map(int, input().split())
+    for n in range(i - 1, j):
+        list[n] = k
+
+for i in list:
+    print(i, end=" ")
