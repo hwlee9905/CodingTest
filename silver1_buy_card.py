1ca2672377f4cdac9a274231b2c31518a613d699
hwlee <9905hyun@naver.com>
2025-01-24 01:19:28 +0900

    solved: 11052번

--- silver1_buy_card.py
+++ silver1_buy_card.py
@@ -4,12 +4,10 @@ INPUT = sys.stdin.readline
 N = int(INPUT())
 P = list(map(int, INPUT().split()))
 P.insert(0,0)
-dp = [0 for _ in range(N + 1)]
+dp = [0] * (N + 1)
 
 for i in range(1, N + 1):
-    if N % i:
-        dp[i] = int((P[i] * (N // i)) + P[(N % i)])
-    else:
-        dp[i] = int(P[i] * (N / i))
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + P[j])
 
-print(max(dp))
+print(dp[-1])
