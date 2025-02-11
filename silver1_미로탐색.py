4cd412e3ab5929c5dd562f21dd0ffe23fd2ea591
hwlee <9905hyun@naver.com>
2025-02-11 21:45:22 +0900

    init: 2178

---
+++ silver1_미로탐색.py
@@ -0,0 +1,9 @@
+# https://www.acmicpc.net/problem/2178
+import sys
+INPUT = sys.stdin.readline
+N, M = map(int, INPUT().split())
+List = [list(map(int, INPUT().strip())) for _ in range(N)]
+
+
+
+print(List)
