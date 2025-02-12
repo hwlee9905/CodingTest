dcab80bba5292b083efadc15e5affea402fabeb4
hwlee <9905hyun@naver.com>
2025-02-12 23:41:00 +0900

    init: 2178

--- silver1_미로탐색.py
+++ silver1_미로탐색.py
@@ -1,9 +1,9 @@
 # https://www.acmicpc.net/problem/2178
-import sys
+import sys, collections
 INPUT = sys.stdin.readline
 N, M = map(int, INPUT().split())
-List = [list(map(int, INPUT().strip())) for _ in range(N)]
-
-
-
+List = [[[0] * (M + 2)] + [[0] + list(map(int, INPUT().strip()))  + [0] for _ in range(N)] + [[0] * (M + 2)]]
+visited = [[0] * (M + 2) for _ in range(N + 2)]
+deque = collections.deque
+queue = deque([1,1,1])
 print(List)
