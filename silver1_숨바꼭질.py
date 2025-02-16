f3c7c6ad7c15f6b16f7af28a5efa84f845e4b780
hwlee <9905hyun@naver.com>
2025-02-16 20:11:40 +0900

    solved: 1697

--- silver1_숨바꼭질.py
+++ silver1_숨바꼭질.py
@@ -1,9 +1,21 @@
 # https://www.acmicpc.net/problem/1697
-import sys
+import sys, collections
 INPUT = sys.stdin.readline
 N, K = map(int, INPUT().split())
+deque = collections.deque
+MAX = 100001
+graph = [0] * MAX
 
-graph = []
+queue = deque([N])
 
-for i in range(N, K + 1):
-    
+while queue:
+    node = queue.popleft()
+    if node == K:
+        print(graph[node])
+        break
+
+    for next_v in (node-1, node+1, 2*node):
+        if 0 <= next_v < MAX and not graph[next_v]:
+            graph[next_v] = graph[node] + 1
+            queue.append(next_v)
+
