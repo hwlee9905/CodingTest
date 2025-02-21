b7844267c76f38ea07044065f1f72fe669fa2272
hwlee <9905hyun@naver.com>
2025-02-22 04:11:55 +0900

    solved: 15686

--- gold5_치킨게임.py
+++ gold5_치킨게임.py
@@ -1,20 +1,22 @@
 # https://www.acmicpc.net/problem/15686
-import sys
+import sys, itertools
+combination = itertools.combinations
 INPUT = sys.stdin.readline
 N, M = map(int, INPUT().split())
 arr = [list(map(int, INPUT().split())) for _ in range(N)]
+MIN = 0
 chickenStore = []
 house = []
-distanceSum = [0] * M
+
 for r in range(N):
     for c in range(N):
         if arr[r][c] == 2:
-            ## !!!!! 각 치킨집이 최소거리인 집들의 최소거리를 모두 합해서 해당 치킨집에 저장해야함
-            chickenStore.append([r,c, 0]) #치킨집 행,열, 해당 치킨집이 최소거리인 집의 개수
-
+            chickenStore.append([r,c]) #치킨집 행,열
         if arr[r][c] == 1:
-            house.append([r,c,0,0])  # 집 행,열, 최소거리인 치킨집 인덱스, 최소거리
+            house.append([r,c])  # 집 행,열
 
+distanceArr = [[0] * len(house) for _ in range(len(chickenStore))]
+minList = [0] * len(house)
 
 for c in range(len(chickenStore)):
     for h in range(len(house)):
@@ -23,20 +25,21 @@ for c in range(len(chickenStore)):
         chickenStoreR = chickenStore[c][0] + 1
         chickenStoreC = chickenStore[c][1] + 1
         distance = abs(houseR - chickenStoreR) + abs(houseC - chickenStoreC)
-        if distance < house[h][3] != 0:
-            chickenStore[c][2] += 1
-            chickenStore[house[h][2]][2] -= 1
-            house[h][2] = c
-            house[h][3] = distance
+        distanceArr[c][h] = distance
 
-        elif house[h][3] == 0:
-            house[h][2] = c
-            house[h][3] = distance
-            chickenStore[c][2] += 1
+comList = list(combination(distanceArr, M))
 
-for h in range(len(house)):
-    distanceSum[house[h][2]] =
+for Lists in comList:
+    for distList in Lists:
+        for a in range(len(distList)):
+            if minList[a] == 0:
+                minList[a] = distList[a]
+            elif minList[a] > distList[a]:
+                minList[a] = distList[a]
+    if MIN == 0:
+        MIN = sum(minList)
+    elif sum(minList) < MIN:
+        MIN = sum(minList)
+    minList = [0] * len(house)
 
-house = sorted(house, key=lambda x: x[3]) # 최소거리 기준으로 집을 정렬
-print(house)
-print(chickenStore)
+print(MIN)
