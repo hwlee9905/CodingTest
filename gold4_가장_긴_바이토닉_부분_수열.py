9f5f2105ebad2f4711d5e6a488052626924f1127
hwlee <9905hyun@naver.com>
2025-03-01 05:25:35 +0900

    solved: 11054

--- gold4_가장_긴_바이토닉_부분_수열.py
+++ gold4_가장_긴_바이토닉_부분_수열.py
@@ -4,8 +4,29 @@ INPUT = sys.stdin.readline
 N = int(INPUT())
 arr = list(map(int, INPUT().split()))
 dp = [1] * N
+dp2 = [1] * N
 current = 0
+maxList = []
+
+for i in range(1, N):
+    for back in range(i):
+        if arr[i] > arr[back]:
+            maxList.append(dp[back] + 1)
+
+    if len(maxList) != 0:
+        dp[i] = max(maxList)
+        maxList =[]
+
+for i in range(N, -1, -1):
+    for back in range(i, N):
+        if arr[i] > arr[back]:
+            maxList.append(dp2[back] + 1)
+
+    if len(maxList) != 0:
+        dp2[i] = max(maxList)
+        maxList =[]
+
 for i in range(N):
+    dp2[i] += dp[i] - 1
 
-
-print(dp)
+print(max(dp2))
